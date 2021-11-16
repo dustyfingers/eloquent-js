a6da0c4f3118d9a3abc48fbfc6dd1086069d00e7
lou <dustyfingers01257@gmail.com>
2021-11-15 18:05:58 -0700

    solved chessboard exercise

--- 2-program-structure/exercises/chessboard.js
+++ 2-program-structure/exercises/chessboard.js
@@ -1,6 +1,21 @@
-let str = '';
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
-    
+let size = 8, str = '';
+
+for (let i = 0; i < size; i++) {
+  let row = '';
+
+  for (let j = 0; j < size; j++) {
+    // if i is even, we do hash/space
+    if (i % 2 === 0) {
+      if (j % 2 === 0) row += '#'
+      else row += ' '
+    }
+    // if i is odd, we do space/hash
+    else {
+      if (j % 2 === 0) row += ' '
+      else row += '#'
+    }
   }
+  str += row + '\n';
 }
+
+console.log(str)
